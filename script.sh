932a622e399e123fb992b09e964226d6c6ef32ee
Vinicius Cardoso <viniciuscc10@gmail.com>
2018-12-11 11:40:21 +0000

    First Commit

---
+++ script.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+
+# Set Colors to Print
+function print_green()
+{
+    echo $(tput setaf 2)$(tput setab 0)$@$(tput sgr0)
+}
+
+function print_blue()
+{
+    echo $(tput setaf 4)$@$(tput sgr0)
+}
+
+function print_red()
+{
+    echo $(tput setaf 1)$@$(tput sgr0)
+}
+
+sudo modprobe industrialio
+print_green "> Module industrilio Up"
+
+sudo modprobe i2c-dev
+print_green "> Module i2c-dev Up"
+
+sudo modprobe i2c-bcm2835
+print_green "> Module i2c-bcm2835 Up"
